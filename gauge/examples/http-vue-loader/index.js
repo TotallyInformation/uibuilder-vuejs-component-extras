b24a36c02c4a254a4005814d1709efe92d93f7c6
Julian Knight <jk.github@totallyinformation.com>
2020-09-20 02:13:38 +0100

    First attempt at updating components direct, no HTML required.

---
+++ gauge/examples/http-vue-loader/index.js
@@ -0,0 +1,204 @@
+/* jshint browser: true, esversion: 5, asi: true */
+/*globals Vue, uibuilder, httpVueLoader */
+// @ts-nocheck
+/*
+  Copyright (c) 2020 Julian Knight (Totally Information)
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+  http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+'use strict'
+
+/** @see https://github.com/TotallyInformation/node-red-contrib-uibuilder/wiki/Front-End-Library---available-properties-and-methods */
+
+// eslint-disable-next-line no-unused-vars
+var app1 = new Vue({
+    el: '#app',
+
+    components: {
+        /** @see https://github.com/FranckFreiburger/http-vue-loader 
+         * By adding a ref="myname" when using in your HTML, you can later reference
+         * the component instance in your code: `this.$refs.myname` refs are not reactive though.
+         */
+        'gauge': httpVueLoader('../uibuilder/vendor/uibuilder-vuejs-component-extras/gauge/gauge.vue'),
+    }, // --- End of components --- //
+
+    data: {
+        /** Use to pass any of vue-svg-gauge properties
+         * @param {gaugeOptions} gaugeConfig
+         * 
+         * @param {String} [gaugeConfig.title] If present, a para is added above the chart. Default: 'uibuilder Gauge',
+         * @param {String} [gaugeConfig.toolTip] If present, adds a title tooltip to the outer element. Default: undefined,
+         * @param {String} [gaugeConfig.figure] If present, a para is added below the chart. Default: undefined,
+         * @param {String} [gaugeConfig.clickEvents] If true, sends data back to uibuilder if clicked. Default: false,
+         * 
+         * @param {Number} [gaugeConfig.value] Gauge value. Optional, use main value prop normally. Default: 0
+         * @param {Number} [gaugeConfig.min] Default: 0,
+         * @param {Number} [gaugeConfig.max] Default: 100,
+         * @param {Number} [gaugeConfig.startAngle] Default: -90,
+         * @param {Number} [gaugeConfig.endAngle] Default: 90,
+         * @param {Number} [gaugeConfig.innerRadius] Default: 60,
+         * @param {Number} [gaugeConfig.separatorStep] Default: 10,
+         * @param {Number} [gaugeConfig.separatorThickness] Default: 4,
+         * @param {Array} [gaugeConfig.gaugeColor] Default: [{ offset: 0, color: '#347AB0' }, { offset: 100, color: '#8CDFAD' }],
+         * @param {String} [gaugeConfig.baseColor] Default: '#DDDDDD',
+         * @param {Number} [gaugeConfig.scaleInterval] Default: 5,
+         * @param {Number} [gaugeConfig.transitionDuration] Default: 1500,
+         * @param {String} [gaugeConfig.easing] Default: 'Circular.Out', @see https://github.com/tweenjs/tween.js/
+         */
+        gaugeConfig: {
+            title: 'My Gauge',
+            //toolTip: 'This is a vue-gauge',
+            clickEvents: true,
+            
+            // :start-angle="-110"
+            // :end-angle="110"
+            // :value="gValue"
+            // :separator-step="1"
+            // :min="0"
+            // :max="10"
+            // :gauge-color="[{ offset: 0, color: '#347AB0'}, { offset: 100, color: '#8CDFAD'}]"
+            // :scale-interval="0.1"
+        },
+        gValue: 0,
+
+        //#region debug
+        startMsg    : 'Vue has started, waiting for messages',
+        feVersion   : '',
+        socketConnectedState : false,
+        serverTimeOffset     : '[unknown]',
+        imgProps             : { width: 75, height: 75 },
+
+        msgRecvd    : '[Nothing]',
+        msgsReceived: 0,
+        msgCtrl     : '[Nothing]',
+        msgsControl : 0,
+
+        msgSent     : '[Nothing]',
+        msgsSent    : 0,
+        msgCtrlSent : '[Nothing]',
+        msgsCtrlSent: 0,
+        //#endregion
+
+    }, // --- End of data --- //
+    computed: {
+        hLastRcvd: function() {
+            var msgRecvd = this.msgRecvd
+            if (typeof msgRecvd === 'string') return 'Last Message Received = ' + msgRecvd
+            else return 'Last Message Received = ' + this.syntaxHighlight(msgRecvd)
+        },
+        hLastSent: function() {
+            var msgSent = this.msgSent
+            if (typeof msgSent === 'string') return 'Last Message Sent = ' + msgSent
+            else return 'Last Message Sent = ' + this.syntaxHighlight(msgSent)
+        },
+        hLastCtrlRcvd: function() {
+            var msgCtrl = this.msgCtrl
+            if (typeof msgCtrl === 'string') return 'Last Control Message Received = ' + msgCtrl
+            else return 'Last Control Message Received = ' + this.syntaxHighlight(msgCtrl)
+        },
+        hLastCtrlSent: function() {
+            var msgCtrlSent = this.msgCtrlSent
+            if (typeof msgCtrlSent === 'string') return 'Last Control Message Sent = ' + msgCtrlSent
+            //else return 'Last Message Sent = ' + this.callMethod('syntaxHighlight', [msgCtrlSent])
+            else return 'Last Control Message Sent = ' + this.syntaxHighlight(msgCtrlSent)
+        },
+    }, // --- End of computed --- //
+    methods: {
+        // return formatted HTML version of JSON object
+        syntaxHighlight: function(json) {
+            json = JSON.stringify(json, undefined, 4)
+            json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;')
+            json = json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, function (match) {
+                var cls = 'number'
+                if (/^"/.test(match)) {
+                    if (/:$/.test(match)) {
+                        cls = 'key'
+                    } else {
+                        cls = 'string'
+                    }
+                } else if (/true|false/.test(match)) {
+                    cls = 'boolean'
+                } else if (/null/.test(match)) {
+                    cls = 'null'
+                }
+                return '<span class="' + cls + '">' + match + '</span>'
+            })
+            return json
+        }, // --- End of syntaxHighlight --- //
+    }, // --- End of methods --- //
+
+    // Available hooks: init,mounted,updated,destroyed
+    mounted: function(){
+        uibuilder.start()
+        var vueApp = this
+        //console.log(vueApp)
+
+        vueApp.feVersion = uibuilder.get('version')
+
+        uibuilder.onChange('msg', function(msg){
+            //console.info('[indexjs:uibuilder.onChange] msg received from Node-RED server:', msg)
+            vueApp.msgRecvd = msg
+            vueApp.msgsReceived = uibuilder.get('msgsReceived')
+
+            if ( msg._uib ) {
+                /** Here we cheat to update the components props directly.
+                 *  This saves users from having to define props and data.
+                 *  This code will be moved into uibuilderfe to make it completely hidden.
+                 *  All you will need is to know the format of data to send.
+                 */
+                if ( msg._uib.componentRef in vueApp.$refs ) {
+                    Object.keys(msg._uib.options).forEach( key => {
+                        //vueApp.gaugeConfig[key] = msg._uib.options[key]
+                        vueApp.$refs[msg._uib.componentRef].$props['config'][key] = msg._uib.options[key]
+                    })
+                    if ( typeof msg.payload === 'number' ) {
+                        console.log(`new gauge value received: ${msg.payload}`)
+                        //vueApp.gValue = msg.payload
+                        vueApp.$refs[msg._uib.componentRef].$props['config']['value'] = msg.payload
+                    }
+                }
+            }
+        })
+
+        //#region
+        uibuilder.onChange('ctrlMsg', function(msg){
+            //console.info('[indexjs:uibuilder.onChange:ctrlMsg] CONTROL msg received from Node-RED server:', msg)
+            vueApp.msgCtrl = msg
+            vueApp.msgsControl = uibuilder.get('msgsCtrl')
+        })
+        uibuilder.onChange('sentMsg', function(msg){
+            //console.info('[indexjs:uibuilder.onChange:sentMsg] msg sent to Node-RED server:', msg)
+            vueApp.msgSent = msg
+            vueApp.msgsSent = uibuilder.get('msgsSent')
+        })
+        uibuilder.onChange('sentCtrlMsg', function(msg){
+            //console.info('[indexjs:uibuilder.onChange:sentCtrlMsg] Control message sent to Node-RED server:', msg)
+            vueApp.msgCtrlSent = msg
+            vueApp.msgsCtrlSent = uibuilder.get('msgsSentCtrl')
+        })
+        uibuilder.onChange('ioConnected', function(connected){
+            //console.info('[indexjs:uibuilder.onChange:ioConnected] Socket.IO Connection Status Changed to:', connected)
+            vueApp.socketConnectedState = connected
+        })
+        uibuilder.onChange('serverTimeOffset', function(serverTimeOffset){
+            //console.info('[indexjs:uibuilder.onChange:serverTimeOffset] Offset of time between the browser and the server has changed to:', serverTimeOffset)
+            vueApp.serverTimeOffset = serverTimeOffset
+        })
+
+        //#endregion ---- End of Trace Sent Messages ---- //
+
+    }, // --- End of mounted hook --- //
+
+}) // --- End of app1 --- //
+
+// EOF
--- gauge/gauge.vue
+++ gauge/gauge.vue
@@ -1,23 +1,26 @@
 <template>
-    <div>
-        <p>Gauge</p>
-        <vue-svg-gauge
-            :start-angle="startAngle"
-            :end-angle="endAngle"
-            :value="value"
-            :separator-step="separatorStep"
-            :min="min"
-            :max="max"
-            :gauge-color="gaugeColor"
-            :scale-interval="scaleInterval"
-        />
+    <div :title="config.toolTip" @click="clickOuterDiv">
+        <p v-if="config.title">
+            {{ config.title }}
+        </p>
+        <vue-svg-gauge 
+            :start-angle="config.startAngle"
+            :end-angle="config.endAngle"
+            :value="value || config.value || 0"
+            :separator-step="config.separatorStep"
+            :min="config.min"
+            :max="config.max"
+            :gauge-color="config.gaugeColor"
+            :scale-interval="config.scaleInterval"
+        ><slot /></vue-svg-gauge>
+        <p v-if="config.figure">{{ config.figure }}</p>
     </div>
 </template>
 
 <script src="../uibuilder/vendor/uibuilder-vuejs-component-extras/node_modules/vue-svg-gauge/dist/vue-svg-gauge.min.js"></script>
 <!-- <script src="https://unpkg.com/vue-svg-gauge@latest/dist/vue-svg-gauge.min.js"></script> -->
 
-<script>
+<script scoped>
     /** Use vue-svg-gauge component module
      * @see https://github.com/hellocomet/vue-svg-gauge
      * @see https://github.com/TotallyInformation/node-red-contrib-uibuilder/wiki/Integrating-vue-svg-gauge
@@ -30,16 +33,37 @@
         name: 'gauge',
 
         /** Note that props with dashes in name have to be referenced as camel case in JS */
-        props: [
-            'start-angle',
-            'end-angle',
-            'value',
-            'separator-step',
-            'min',
-            'max',
-            'gauge-color',
-            'scale-interval',
-        ], // -- End of props -- //
+        props: {
+            'value': Number,
+            'config': {
+                type: Object,
+                default: function() { return {
+                    'title': 'uibuilder Gauge',
+                    'toolTip': undefined,
+                    'figure': undefined,
+                    'clickEvents': false,
+
+                    /** Native vue-svg-gauge props, @see https://github.com/hellocomet/vue-svg-gauge#props
+                     * Native defaults:
+                     * 'value': 70,
+                     * 'min': 0,
+                     * 'max': 100,
+                     * 'start-angle': -90,
+                     * 'end-angle': 90,
+                     * 'inner-radius': 60,
+                     * 'separator-step': 10,
+                     * 'separator-thickness': 4,
+                     * 'gauge-color': [{ offset: 0, color: '#347AB0' }, { offset: 100, color: '#8CDFAD' }],
+                     * 'base-color': '#DDDDDD',
+                     * 'scale-interval': 5,
+                     * 'transition-duration': 1500,
+                     * 'easing': 'Circular.Out',
+                     *    For animation settings, @see https://github.com/tweenjs/tween.js/
+                     */
+                    'value': 0,
+                } },
+            },
+        }, // -- End of props -- //
 
         data: function() { return {
         }}, // -- End of data -- //
@@ -48,13 +72,49 @@
         }, // -- End of computed -- //
 
         methods: {
+            // https://stackoverflow.com/questions/48250832/programmatically-bind-custom-events-for-dynamic-components-in-vuejs/48251122
+            // evtHandler: function(e){
+            //     console.log('evtHandler', e)
+            // },
+            clickOuterDiv: function(e){
+                if ( this.config.clickEvents !== true ) return
+                
+                console.log('clickOuterDiv - e', e)
+                console.log('clickOuterDiv - this', this)
+
+                if ( window.uibuilder ) uibuilder.send({
+                    'topic': 'gauge clicked',
+                    //'payload': e,  // for some reason, just passing e only gives e.isTrusted and nothing else
+                    'payload': {
+                        x: e.x, y: e.y,
+                        clientX: e.clientX, clientY: e.clientY, pageX: e.pageX, pageY: e.pageY,  offsetX: e.offsetX, offsetY: e.offsetY,  layerX: e.layerX, layerY: e.layerY,  screenX: e.screenX, screenY: e.screenY, 
+
+                        altKey: e.altKey, ctrlKey: e.ctrlKey, metaKey: e.metaKey, shiftKey: e.shiftKey,
+
+                        componentRef: this.$vnode.data.ref,
+                        componentTag: this.$vnode.componentOptions.tag,
+                        eventType: e.type,
+                    },
+                })
+            },
         }, // -- End of methods -- //
 
-        //   components: {
-        //     //VueSvgGauge,
-        //     'VueSvgGauge': httpVueLoader('../uibuilder/vendor/uibuilder-vuejs-component-extras/node_modules/vue-svg-gauge/dist/vue-svg-gauge.min.js'),
-        //   }
+        // Available hooks: init,mounted,updated,destroyed
+        mounted: function(){
+            const myComponent = this
+
+            console.log(myComponent)
+
+            // Check if uibuilder is being used
+            if ( window.uibuilder ) {
+                console.log('GREAT! You are using uibuilder, thanks!')
+
+                // Dynamically add event handler
+                //myComponent.$el.addEventListener('click', myComponent.clickOuterDiv)
+            }
+        },
     }
 </script>
 
-<style></style>
+<style>
+</style>
