b1b645395701ad6159d01b5e1c7fa3895bf3d7f3
Julian Knight <jk.github@totallyinformation.com>
2020-09-21 22:41:50 +0100

    Semantic HTML, style prop, html header/caption, CSS, tidy up.

--- gauge/gauge.vue
+++ gauge/gauge.vue
@@ -1,9 +1,10 @@
 <template>
-    <div :title="config.toolTip" @click="clickOuterDiv">
-        <p v-if="config.title">
-            {{ config.title }}
-        </p>
+    <figure :title="config.toolTip" @click="clickOuter">
+        <figcaption v-if="config.header" class="uibg-t">
+            <span v-html="config.header" />
+        </figcaption>
         <vue-svg-gauge 
+            :style="config.style"
             :start-angle="config.startAngle"
             :end-angle="config.endAngle"
             :value="value || config.value || 0"
@@ -12,10 +13,26 @@
             :max="config.max"
             :gauge-color="config.gaugeColor"
             :scale-interval="config.scaleInterval"
-        ><slot /></vue-svg-gauge>
-        <p v-if="config.figure">{{ config.figure }}</p>
-    </div>
+        >
+            <slot />
+        </vue-svg-gauge>
+        <figcaption v-if="config.caption" class="uibg-b">
+            <span v-html="config.caption" />
+        </figcaption>
+    </figure>
 </template>
+
+<style scoped>
+    figcaption {
+        text-align: center;
+    }
+    .uibg-t {
+        margin-bottom: 1rem;
+    }
+    .uibg-b {
+        margin-top: 1rem;
+    }
+</style>
 
 <script src="../uibuilder/vendor/uibuilder-vuejs-component-extras/node_modules/vue-svg-gauge/dist/vue-svg-gauge.min.js"></script>
 <!-- <script src="https://unpkg.com/vue-svg-gauge@latest/dist/vue-svg-gauge.min.js"></script> -->
@@ -25,8 +42,6 @@
      * @see https://github.com/hellocomet/vue-svg-gauge
      * @see https://github.com/TotallyInformation/node-red-contrib-uibuilder/wiki/Integrating-vue-svg-gauge
      */
-
-    //import { VueSvgGauge } from 'vue-svg-gauge'
 
     module.exports = {
         //export default {
@@ -38,10 +53,11 @@
             /** Use to pass any of vue-svg-gauge properties
              * @param {gaugeOptions} config
              * 
-             * @param {String} [config.title] If present, a para is added above the chart. Default: 'uibuilder Gauge',
+             * @param {String} [config.header] If present, a para is added above the chart. Default: 'uibuilder Gauge',
              * @param {String} [config.toolTip] If present, adds a title tooltip to the outer element. Default: undefined,
-             * @param {String} [config.figure] If present, a para is added below the chart. Default: undefined,
+             * @param {String} [config.caption] If present, a para is added below the chart. Default: undefined,
              * @param {String} [config.clickEvents] If true, sends data back to uibuilder if clicked. Default: false,
+             * @param {String} [config.style] CSS styles to override the gauge style. Default: "height:15rem;",
              * 
              * @param {Number} [config.value] Gauge value. Optional, use main value prop normally. Default: 0
              * @param {Number} [config.min] Default: 0,
@@ -51,7 +67,9 @@
              * @param {Number} [config.innerRadius] Default: 60,
              * @param {Number} [config.separatorStep] Default: 10,
              * @param {Number} [config.separatorThickness] Default: 4,
-             * @param {Array} [config.gaugeColor] Default: [{ offset: 0, color: '#347AB0' }, { offset: 100, color: '#8CDFAD' }],
+             * @param {Object[]} [config.gaugeColor] Default: [{ offset: 0, color: '#347AB0' }, { offset: 100, color: '#8CDFAD' }],
+             * @param {Number} [config.gaugeColor[].offset] Starting value for the colour.
+             * @param {Number} [config.gaugeColor[].color]  Colour to apply at starting offset. Any valid CSS colour specification.
              * @param {String} [config.baseColor] Default: '#DDDDDD',
              * @param {Number} [config.scaleInterval] Default: 5,
              * @param {Number} [config.transitionDuration] Default: 1500,
@@ -60,10 +78,11 @@
             'config': {
                 type: Object,
                 default: function() { return {
-                    'title': 'uibuilder Gauge',
+                    'header': undefined,
                     'toolTip': undefined,
-                    'figure': undefined,
+                    'caption': undefined,
                     'clickEvents': false,
+                    'style': 'height:15rem;', // fix the default height to something reasonable
 
                     /** Native vue-svg-gauge props, @see https://github.com/hellocomet/vue-svg-gauge#props
                      * Native defaults:
@@ -87,60 +106,31 @@
             },
         }, // -- End of props -- //
 
-        data: function() { return {
-        }}, // -- End of data -- //
-
-        computed: {
-        }, // -- End of computed -- //
-
         methods: {
-            // https://stackoverflow.com/questions/48250832/programmatically-bind-custom-events-for-dynamic-components-in-vuejs/48251122
-            // evtHandler: function(e){
-            //     console.log('evtHandler', e)
-            // },
-            clickOuterDiv: function(e){
+            /** Handle click events
+             * @param {Object} e DOM/Vue event data
+             */
+            clickOuter: function(e){
                 // Only respond to clicks if needed
                 if ( this.config.clickEvents !== true ) return
                 
-                console.log('clickOuterDiv - e', e)
-                console.log('clickOuterDiv - this', this)
-
-                /** If this component is being used with Node-RED/uibuilder,
-                 *  Send a msg back to Node-RED.
-                 */
+                /** If this component is being used with Node-RED/uibuilder, Send a msg back to Node-RED. */
                 if ( window.uibuilder ) uibuilder.send({
                     'topic': 'gauge clicked',
                     // For some reason, just passing e only gives e.isTrusted and nothing else
                     'payload': {
+                        componentRef: this.$vnode.data.ref,
+                        componentTag: this.$vnode.componentOptions.tag,
+                        eventType: e.type,
+
                         x: e.x, y: e.y,
                         clientX: e.clientX, clientY: e.clientY, pageX: e.pageX, pageY: e.pageY,  offsetX: e.offsetX, offsetY: e.offsetY,  layerX: e.layerX, layerY: e.layerY,  screenX: e.screenX, screenY: e.screenY, 
 
                         altKey: e.altKey, ctrlKey: e.ctrlKey, metaKey: e.metaKey, shiftKey: e.shiftKey,
-
-                        componentRef: this.$vnode.data.ref,
-                        componentTag: this.$vnode.componentOptions.tag,
-                        eventType: e.type,
                     },
                 })
             },
         }, // -- End of methods -- //
 
-        // Available hooks: init,mounted,updated,destroyed
-        mounted: function(){
-            const myComponent = this
-
-            console.log(myComponent)
-
-            // Check if uibuilder is being used
-            if ( window.uibuilder ) {
-                console.log('GREAT! You are using uibuilder, thanks!')
-
-                // Dynamically add event handler
-                //myComponent.$el.addEventListener('click', myComponent.clickOuterDiv)
-            }
-        },
     }
 </script>
-
-<style>
-</style>
